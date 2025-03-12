5c76c121315fa3a213744ded80becdfeecc70d5e
EuanRobertson1 <92741298+EuanRobertson1@users.noreply.github.com>
2025-03-12 17:36:23 +0000

    some  PWA fixes

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,19 @@
-const CACHE_NAME = "session-plus-cache";
-const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/styles.css",
-    "/app.js",
-    "/manifest.json",
-    "/icons/icon-192x192.png"
-];
-
-self.addEventListener("install", (event) => {
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(FILES_TO_CACHE);
+        caches.open('app-cache').then((cache) => {
+            return cache.addAll([
+                './index.html',
+                './styles.css',
+                './app.js',
+                './manifest.json',
+                './icons/icon-192x192.png',
+                './icons/icon-512x512.png'
+            ]);
         })
     );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
             return response || fetch(event.request);
